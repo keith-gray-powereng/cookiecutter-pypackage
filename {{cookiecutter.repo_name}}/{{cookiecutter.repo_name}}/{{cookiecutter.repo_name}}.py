499e5e7e47c9c1678a16c924e89211e4b2200888
Keith Gray <keith.gray@powereng.com>
2015-01-28 10:49:03 -0700

    Added skeleton of command line parsing to the project

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}.py
@@ -1,1 +1,17 @@
 # -*- coding: utf-8 -*-
+from argparse import ArgumentParser
+import sys
+
+
+def main():
+    parser = ArgumentParser(
+        description='{{ cookiecutter.project_short_description }}'
+    )
+
+    # Build the command line argument parser here
+
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
